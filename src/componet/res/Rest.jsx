3c9a13fe5d3e36b4463e38873f04bcd2520b1714
wasan <wasanawwad2020@gmail.com>
2023-10-31 15:48:33 +0200

    edit

--- src/componet/res/Rest.jsx
+++ src/componet/res/Rest.jsx
@@ -1,24 +1,42 @@
 import React, {useEffect, useState} from 'react'
 
 
+
 function Rest() {
-  return (
-    <>
-    <div className=" row ms-auto">
-    <section className='pizza'>
-      <Link to ="/pizza"> Pizza</Link>
-    </section>
+  let [Recipes,setRecipes] = useState([]);
+    
+  const getRecipes = async ()=>{
+      let reponse = await fetch("https://forkify-api.herokuapp.com/api/search?q=pizza");
+      let data = await reponse.json();
+      setRecipes(data);
+     // console.log(data);
+  }
 
-    <section className=' Onion'>
-      <Link to ="/onion"> Onion</Link>
-    </section>
+      useEffect(() =>{
+         getRecipes();
+         
+},[])
 
-    <section className=' salad'>
-      <Link to ="/salad"> salad</Link>
-    </section>
-    </div>
-    </>
-  )
+
+return (
+  <>
+  
+      <section className=' row pizza'>
+      {
+      Recipes.map((ele)=>{
+          return <div className='col-md-4' key={ele.id}>
+              <h2>{ele.title}</h2>
+              <img src={ele.image} className='w-100'/>
+          </div>
+  
+      } )}
+      </section>
+      
+  </>
+  
+     
+  
+)
 }
 
 export default Rest
